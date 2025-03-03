e3fec4fc4e2e74f9aa0c89c7e825e834185dda06
Talha-Shaikh1 <Talha369852@gmail.com>
2025-03-03 15:55:26 +0500

    day 5 challenge

---
+++ 05_sum_n_numbers.py
@@ -0,0 +1,15 @@
+'''
+📢 Day 5 – Daily Python Challenge 🐍
+🚀 Challenge:
+Aisa Python program likhna hai jo user se ek number lega aur uska sum calculate karega 1 se lekar us number tak. 🔢💡
+
+🔥 Example:
+📌 Input: 5
+📌 Output: Sum: 15 (kyunki 1 + 2 + 3 + 4 + 5 = 15)
+
+'''
+num = int(input("Enter a num: "))
+
+print(f"The sum of number from 1 to {num} is: {sum(range(1, num+1))}")
+
+
