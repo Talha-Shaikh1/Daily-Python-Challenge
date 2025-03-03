2161103fe7bb3f7d82b2187d02e4b317b355b20c
Talha-Shaikh1 <Talha369852@gmail.com>
2025-03-03 10:58:55 +0500

    day-4-challenge

---
+++ 04_number_into_word.py
@@ -0,0 +1,20 @@
+'''
+📢 Day 4 – Daily Python Challenge 🐍
+🚀 Challenge:Write a Python program that converts numbers into words! 🔢➡🔡
+
+🔥Example:
+📌 Input: 123
+📌 Output: "One Hundred Twenty-Three"
+
+📌 Input: 5067
+📌 Output: "Five Thousand Sixty-Seven"
+
+'''
+
+
+from num2words import num2words
+
+num = int(input("Enter a number: "))
+
+words = num2words(num)
+print(words)
