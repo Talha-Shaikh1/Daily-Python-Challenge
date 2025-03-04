be4d6a239f451f75251f41091a7dae0fab1c39a7
Talha-Shaikh1 <Talha369852@gmail.com>
2025-03-04 21:46:46 +0500

    update

---
+++ 07_todo_list.py
@@ -0,0 +1,23 @@
+'''
+📢 Day 7 – Daily Python Challenge 🐍
+🚀 Challenge:
+Aaj ka challenge ek mini project hoga! Tumhe ek "To-Do List CLI App" banani hai jo user se tasks store kare, show kare aur delete bhi kar sake! 📝✅❌
+
+🔥 Requirements:
+⿡ Task Add Karein: User new task input kare aur list me add ho jaye.
+⿢ Task Show Karein: Jo tasks add hue hain, unko show karein.
+⿣ Task Delete Karein: User kisi task ko delete kar sake.
+⿤ Quit Option: Program exit hone ka option bhi ho.
+
+📌 Example:
+
+Welcome to To-Do List!
+1. Add Task
+2. Show Tasks
+3. Delete Task
+4. Exit
+Enter choice: 1
+Enter new task: Complete Python Challenge
+Task Added!
+
+'''
