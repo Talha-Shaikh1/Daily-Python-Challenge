f54874c62c6b65627852502b8754f86cd9c82944
Talha-Shaikh1 <Talha369852@gmail.com>
2025-03-04 21:49:53 +0500

    update

---
+++ 09_pass_strength.checker.py
@@ -0,0 +1,16 @@
+'''
+📢 Day 9 – Daily Python Challenge 🐍
+🚀 Challenge: "Password Strength Checker" 🔐
+Aaj ka challenge hai ek password strength checker banana! Tumhe ek Python program likhna hai jo user ke diye gaye password ki strength analyze kare aur bata sake ke password weak, moderate ya strong hai! 💪🔑
+
+🔥 Requirements:
+⿡ User se password input lo.
+⿢ Check karo ke password kitna strong hai:
+
+Weak: Agar password 6 characters se chhota hai ya sirf alphabets hain.
+
+Moderate: Agar password 8+ characters ka hai lekin koi special character nahi hai.
+
+Strong: Agar password 8+ characters, uppercase, lowercase, number, aur special character sab kuch contain karta hai.
+
+'''
