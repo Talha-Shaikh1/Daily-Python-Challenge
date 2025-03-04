f24d577ed78ff3ac5d0406bde04151088fb76328
Talha-Shaikh1 <Talha369852@gmail.com>
2025-03-04 21:48:32 +0500

    update

---
+++ 08_num_guessing_game.py
@@ -0,0 +1,14 @@
+'''
+📢 Day 8 – Daily Python Challenge 🐍  
+
+🚀 Challenge: 
+Aaj ka challenge "Number Guessing Game" hai! 🎯 Tumhe ek Python program likhna hai jo ek random number (1-100) generate kare aur user ko us number ko guess karne ka chance de! 💡🔢  
+
+🔥 Requirements: 
+⿡ Random number generate karein (1-100 ke beech).
+⿢ User se guess input lein.  
+⿣ Agar guess sahi ho to "Congratulations! You guessed it right!" print karein. 
+⿤ Agar galat ho to batao ke number chhota hai ya bada.  
+⿥ User ko multiple attempts milne chahiye.
+
+'''
